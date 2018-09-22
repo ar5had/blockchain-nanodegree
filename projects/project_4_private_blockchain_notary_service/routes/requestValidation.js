169e88485b76e6a4f1c7797ac490023ddafa50e3
Arshad Khan <arshdkhn1@gmail.com>
2018-09-22 21:12:09 +0530

    ...

--- projects/project_4_private_blockchain_notary_service/routes/requestValidation.js
+++ projects/project_4_private_blockchain_notary_service/routes/requestValidation.js
@@ -2,38 +2,44 @@ const express = require('express')
 const router = express.Router()
 
 const requestValidationRoutes = (app, starRegistry) => {
-app.post('/requestValidation', (req, res) => {
-    const { address } = req.body
+    app.post('/requestValidation', (req, res) => {
+        const { address } = req.body
 
-    if (address === '') return res.send(`Address is not valid!`)
+        if (address === '') return res.send(`Address is not valid!`)
 
-    const timestamp = new Date().getTime()
-    const message = `${address}:${timestamp}:starRegistry`
-    const validationWindow = 300
-    const json = {
-        address,
-        message,
-        validationWindow,
-        requestTimeStamp: timestamp
-    }
-
-    if (starRegistry.hasOwnProperty(address)) {
-        return res.json(Object.assign({}, json, {requestTimeStamp: starRegistry[address].timestamp}))
-    }
-
-    starRegistry[address] = { message, timestamp, validationWindow, isVerified: false }
-    res.json(json)
-
-    const interval = setInterval(() => {
-        starRegistry[address] && starRegistry[address].validationWindow--
-        // check if starRegistry has star registeration request or not
-        // if there is a registeration request
-        // then check whether the validationWindow is timed out or not
-        if (!starRegistry.hasOwnProperty(address) || starRegistry[address].validationWindow <= 0) {
-            delete starRegistry[address]
-            clearInterval(interval)
+        const timestamp = new Date().getTime()
+        const message = `${address}:${timestamp}:starRegistry`
+        const validationWindow = 300
+        const json = {
+            address,
+            message,
+            validationWindow,
+            requestTimeStamp: timestamp
         }
-    }, 1000)
-})
+
+        if (starRegistry.hasOwnProperty(address)) {
+            return res.json(
+                Object.assign(
+                    {},
+                    json,
+                    { validationWindow: starRegistry[address].validationWindow }
+                )
+            )
+        }
+
+        starRegistry[address] = { message, timestamp, validationWindow, isVerified: false }
+        res.json(json)
+
+        const interval = setInterval(() => {
+            starRegistry[address] && starRegistry[address].validationWindow--
+            // check if starRegistry has star registeration request or not
+            // if there is a registeration request
+            // then check whether the validationWindow is timed out or not
+            if (!starRegistry.hasOwnProperty(address) || starRegistry[address].validationWindow <= 0) {
+                delete starRegistry[address]
+                clearInterval(interval)
+            }
+        }, 1000)
+    })
 }
 module.exports = requestValidationRoutes
